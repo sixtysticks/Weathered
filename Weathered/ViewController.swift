9158561c9f5ccccc80817f361321190ba1feb7c2
David Gibbs <david.gibbs@dressipi.com>
2015-12-07 20:52:35 +0000

    Initial Commit

---
+++ Weathered/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weathered
+//
+//  Created by David Gibbs on 07/12/2015.
+//  Copyright © 2015 SixtySticks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
